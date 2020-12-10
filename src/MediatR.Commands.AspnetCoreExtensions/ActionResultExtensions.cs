b847283bce1599c4484aff7bfa6915fceff7974c
Daniel Portugal <dpcosta@gmail.com>
2020-12-10 08:55:26 -0300

    Import inicial

---
+++ src/MediatR.Commands.AspnetCoreExtensions/ActionResultExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MediatR.Commands.Results;
+using MediatR.Commands.BodyTypes;
+
+namespace MediatR.Commands.AspnetCoreExtensions
+{
+    public static class ActionResultExtensions
+    {
+        public static IActionResult ToActionResult(this IResult result)
+        {
+            Type resultType = result.GetType();
+            
+            if (resultType.IsGenericType)
+            {
+                Type genericTypeDefinition = resultType.GetGenericTypeDefinition();
+                if (genericTypeDefinition == typeof(CreatedResult<>)) return new OkObjectResult(result.Body) { StatusCode = 201 };
+                if (genericTypeDefinition == typeof(DeletedResult<>)) return new NoContentResult();
+            }
+            
+            if (result is Results.NotFoundResult) return new Microsoft.AspNetCore.Mvc.NotFoundResult();
+            if (result is Results.UnauthorizedResult) return new UnauthorizedObjectResult(result.Body);
+            if (result is Results.ForbiddenResult) return new StatusCodeResult(403);
+            if (result is InvalidInputResult) return new BadRequestObjectResult(result.Body);
+            if (result is ExceptionResult)
+            {
+                var e = result.Body as ExceptionBody;
+                return new OkObjectResult(e.Exception.Message) { StatusCode = 500 };
+            }
+            return new OkObjectResult(result.Body);
+        }
+    }
+}
---
+++ tests/MediatR.Commands.AspnetCoreExtensions.Tests/ActionResultExtensionsToActionResult.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using MediatR.Commands.Results;
+
+namespace MediatR.Commands.AspnetCoreExtensions.Tests
+{
+    public class ActionResultExtensionsToActionResult
+    {
+        public static IEnumerable<object[]> ResultTypeInputAndExpectedActionResultType => new List<object[]>
+        {
+            new object[] { new CreatedResult<string>("Testes"), typeof(OkObjectResult) },
+            new object[] { new UpdatedResult<string>("Testes"), typeof(OkObjectResult) },
+            new object[] { new DeletedResult<string>("Testes"), typeof(NoContentResult) },
+            new object[] { new Results.NotFoundResult(), typeof(Microsoft.AspNetCore.Mvc.NotFoundResult) },
+            new object[] { new Results.UnauthorizedResult(), typeof(UnauthorizedObjectResult) },
+            new object[] { new InvalidInputResult("Testes"), typeof(BadRequestObjectResult) },
+            new object[] { new ExceptionResult(new Exception("Testes")), typeof(OkObjectResult) }
+        };
+
+        public static IEnumerable<object[]> ResultTypeInputAndExpectedStatusCode => new List<object[]>
+        {
+            new object[] { new CreatedResult<string>("Testes"), 201 },
+            new object[] { new UpdatedResult<string>("Testes"), 200 },
+            new object[] { new DeletedResult<string>("Testes"), 204 },
+            new object[] { new Results.NotFoundResult(), 404 },
+            new object[] { new Results.UnauthorizedResult(), 401 },
+            new object[] { new InvalidInputResult("Testes"), 400 },
+            new object[] { new ExceptionResult(new Exception("Testes")), 500 }
+        };
+
+        [Theory]
+        [MemberData(nameof(ResultTypeInputAndExpectedActionResultType))]
+        public void ShouldConvertToExpectedActionResultType(IResult commandResult, Type expectedType)
+        {
+            // arrange: from method arguments
+
+            // act
+            var result = commandResult.ToActionResult();
+
+            // assert
+            Assert.IsType(expectedType, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(ResultTypeInputAndExpectedStatusCode))]
+        public void ShouldReturnExpectedStatusCode(IResult commandResult, int expectedStatusCode)
+        {
+            // arrange from method arguments
+
+            // act
+            var result = commandResult.ToActionResult();
+            var statusCode = (int)result.GetType().GetProperty("StatusCode").GetValue(result);
+
+            // assert
+            Assert.Equal(expectedStatusCode, statusCode);
+        }
+    }
+}
